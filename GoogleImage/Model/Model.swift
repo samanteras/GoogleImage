ed7e186972b9320682da8c5cf0a02c481803a46d
samanteras <samanter@mail.ru>
2023-02-07 23:57:35 +0300

    Get json data

---
+++ GoogleImage/Model/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  GoogleImage
+//
+//  Created by MAC13 on 07.02.2023.
+//
+
+import Foundation
+import UIKit
+
+
+struct APIResponse: Decodable{
+    var images_results: [Result]
+}
+
+struct Result: Codable{
+    var thumbnail: String
+    var link: String
+}
+
--- GoogleImage/ViewController.swift
+++ GoogleImage/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images_results: [Result] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        request()
+        
+    }
+    
+    func request(){
+        guard let url = URL(string: "https://serpapi.com/search.json?q=Apple&tbm=isch&ijn=0") else {
+            return
+        }
+     //   guard let self = self else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+        
+            guard let data = data, error == nil  else {
+                return
+            }
+            
+            do {
+                let jsonResult = try JSONDecoder().decode(APIResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self.images_results = jsonResult.images_results
+                }
+                print(jsonResult.images_results.count)
+            }
+            catch {
+                print(error)
+            }
+        }
+        task.resume()
     }
 
 
 }
+
 
