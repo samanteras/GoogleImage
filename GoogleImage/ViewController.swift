d448d55717d454aa6145103cf10c4ee9d1bdb10a
samanteras <samanter@mail.ru>
2023-02-07 17:31:39 +0300

    Create project

---
+++ GoogleImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogleImage
+//
+//  Created by MAC13 on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
